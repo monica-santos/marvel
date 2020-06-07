ee776891b3a7da425129597fb2f4400a2e223570
Mônica Santos <monicasantosdev@gmail.com>
2020-06-07 01:02:59 -0300

    :lipstick: (components) add category component

---
+++ src/components/Category/index.tsx
@@ -0,0 +1,44 @@
+import React, { useContext } from "react"
+import { ViewProps } from "react-native"
+import { LinearGradient } from "expo-linear-gradient"
+import { useTheme, DefaultTheme, ThemeContext } from "styled-components"
+
+import Icon from "../Icons"
+
+type CategoryProps = {
+  name: "human" | "alien" | "antihero" | "villain" | "hero"
+}
+
+type Props = CategoryProps & ViewProps & DefaultTheme
+const mapGradient = {
+  hero: "blue",
+  villain: "red",
+  antihero: "purple",
+  alien: "green",
+  human: "pink",
+}
+
+const Category: React.FC<Props> = ({ name }) => {
+  const theme = useTheme()
+
+  // TODO: fix this
+  // @ts-ignore
+  const { colors } = theme.gradients[mapGradient[name]]
+
+  return (
+    <LinearGradient
+      style={{
+        width: 56,
+        height: 56,
+        borderRadius: 28,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      colors={colors}
+    >
+      <Icon name={name} width={32} height={32} />
+    </LinearGradient>
+  )
+}
+
+export default Category
